357b919f2abb65fd9d7d7d4799c1bef9dd543553
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-12-13 20:51:21 -0300

    Rollback

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <v-app>
+    <v-app-bar
+      app
+      dense
+      dark
+      color="blue-grey"
+      class="px-3"
+    >
+      <div class="d-flex align-center">
+        <v-btn
+          depressed
+          dense
+          color="blue-grey"
+          :to="{ name: 'home' }"
+        >
+          THE PROFILE INDEXER
+        </v-btn>
+      </div>
+      <v-spacer></v-spacer>
 
+      <v-btn
+        href="https://github.com/EzequielDeOliveira/Trabalho-Individual-2020-1"
+        target="_blank"
+        icon
+      >
+        <v-icon>fab fa-github</v-icon>
+      </v-btn>
+    </v-app-bar>
 
     <v-main>
       <router-view/>
